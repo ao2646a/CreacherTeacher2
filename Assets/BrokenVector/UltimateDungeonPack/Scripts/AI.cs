b423cc84296927f6a4f8f784fed1f0607d446648
Elsa Sjogren <sjogea19@wfu.edu>
2021-11-30 13:02:35 +0100

    changed the liquid mechanics using rigid bodies

---
+++ Assets/BrokenVector/UltimateDungeonPack/Scripts/AI.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AIState
+{
+    Roaming,
+    Idle,
+    Attack
+}
+
+public class AI : MonoBehaviour
+{
+    private AIState state;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = AIState.Idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case AIState.Roaming:
+                break;
+            case AIState.Idle:
+                IdleBehavior();
+                break;
+            case AIState.Attack:
+                break;
+        }
+    }
+
+    void IdleBehavior() {
+        state = AIState.Roaming;
+    }
+
+    public void CHnageToRoaming()
+    {
+        state = AIState.Roaming;
+    }
+
+}
